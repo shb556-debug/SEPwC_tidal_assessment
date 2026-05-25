1e673b8d409785cf15bbe964450519d74422bf30
Charlotte Jackson <shb556@york.ac.uk>
2026-05-25 12:23:39 +0100

    fixed all issues with the first function

--- tidal_analysis.py
+++ tidal_analysis.py
@@ -12,56 +12,12 @@ import argparse
 
 
 def read_tidal_data(filename):
-
-    return
-    
-def extract_single_year_remove_mean(year, data):
-
-    return 
-
-
-def extract_section_remove_mean(start, end, data):
-
-    return year_data
-
-
-def join_data(data1, data2):
-
-    return 
-
-def sea_level_rise(data):
-
-    return
-
-def tidal_analysis(data, constituents, start_datetime):
-
-    return
-
-def get_longest_contiguous_data(data):
-
-    return 
-
-
-def main(args_list=None):
-
-    parser = argparse.ArgumentParser(
-                     prog="UK Tidal analysis",
-                     description="Calculate tidal constiuents and RSL from tide gauge data",
-                     )
-
-    parser.add_argument("directory",
-                    help="the directory containing txt files with data")
-    parser.add_argument('-v', '--verbose',
-                    action='store_true',
-                    default=False,
-                    help="Print progress")
-
-    args = parser.parse_args(args_list)
-    dirname = args.directory
-    verbose = args.verbose
-
-    print("Add your code here to do things!")
-    
-
-if __name__ == '__main__':
-    main()
+    tide_Data = pd.read_csv("C:Onedrive/Documents/Code/data", header= None)
+    tide_Data['Date'] = pd.to_datetime(dict(year=tide_Data[0], month=tide_Data[1], day=tide_Data[2], hour=tide_Data[3], minute=tide_Data[4], seconds= tide_Data[5]))
+    # col0 is year, col 1 is month, col 2 is day col 3 is hour col 4 is minute, col 5 is seconds 
+    tide_Data= tide_Data.drop([0,1,2,3,4,5], axis=1)
+    tide_Data= tide_Data.rename(columns={6: "tide"})
+    tide_Data= tide_Data.set_index('Date')
+    tide_Data= tide_Data.mask(tide_Data['Tide']< -300)
+    return tide_Data
+     
